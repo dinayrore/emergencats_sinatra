23776ef2c258bf3e087141df4d0ee699ab4a0655
Kristine <kteich88@gmail.com>
2016-10-14 23:37:14 -0400

    Styling page and pics to match cats in database. Toying with ideas on how to connect front-end to back-end.

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,51 @@
+// jQuery for page scrolling feature
+$('.nav').click(function() {
+  var id = $(this).attr('id');
+  $('html, body').animate({
+      scrollTop: ($('#' + id + '.section').offset().top)
+  }, 1000);
+});
+
+
+
+$('#create-').click(function() {
+  var user = $('#user-id').val()
+  var title = $('#task-title').val()
+  var status = $('#status').val()
+  var priority = $('#priority').val()
+
+
+
+  var closeButton = $('#close');
+  var createCat = $('#add');
+
+  // Function To Display Popup
+  function divShow() {
+    $('#form').show();
+  }
+  // Open up Popup
+  createCat.click(function() {
+    divShow();
+  });
+  // Function to Hide Popup
+  function divHide(){
+  // document.getElementById('form').style.display = "none";
+    $('#form').hide();
+  }
+  // Close out of Popup
+  closeButton.click(function() {
+    divHide();
+  });
+
+
+
+
+
+
+
+
+
+
 var $catName = $('.catName')
 
 function createCats () {
