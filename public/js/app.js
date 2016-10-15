1640a1d7b787239dd9c168578f5b7b2c397ccb94
Kristine <kteich88@gmail.com>
2016-10-15 12:37:14 -0400

    Made several formatting changes. Not sure how to make title happen, left comment. Working on JS and button clicks connecting to database.

--- public/js/app.js
+++ public/js/app.js
@@ -8,43 +8,11 @@ $('.nav').click(function() {
 
 
 
-$('#create-').click(function() {
+$('#create').click(function() {
   var user = $('#user-id').val()
   var title = $('#task-title').val()
   var status = $('#status').val()
   var priority = $('#priority').val()
-
-
-
-  var closeButton = $('#close');
-  var createCat = $('#add');
-
-  // Function To Display Popup
-  function divShow() {
-    $('#form').show();
-  }
-  // Open up Popup
-  createCat.click(function() {
-    divShow();
-  });
-  // Function to Hide Popup
-  function divHide(){
-  // document.getElementById('form').style.display = "none";
-    $('#form').hide();
-  }
-  // Close out of Popup
-  closeButton.click(function() {
-    divHide();
-  });
-
-
-
-
-
-
-
-
-
 
 var $catName = $('.catName')
 
